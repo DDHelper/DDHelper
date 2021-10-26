0a3e094d586e7edef3226200895467371abbbf4d
EdrowsLuo <793971297@qq.com>
2021-10-26 16:32:08 +0800

    添加测试

--- account/tests.py
+++ account/tests.py
@@ -1,3 +1,47 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+
+class AccountTest(TestCase):
+    def test_register(self):
+        response = self.client.post(
+            "/account/register/",
+            {
+                "username": "test_account",
+                "password": "123456"
+            })
+        self.assertEqual(response.status_code, 200)
+
+        response = self.client.post(
+            "/account/register/",
+            {
+                "username": "test_account",
+                "password": "123456"
+            })
+        self.assertEqual(response.status_code, 403)
+
+    def test_register_and_login(self):
+        response = self.client.post(
+            "/account/register/",
+            {
+                "username": "test_account",
+                "password": "123456"
+            })
+        self.assertEqual(response.status_code, 200)
+
+        response = self.client.post(
+            "/account/login/",
+            {
+                "username": "test_account",
+                "password": "test"
+            })
+        self.assertEqual(response.status_code, 403)
+
+        response = self.client.post(
+            "/account/login/",
+            {
+                "username": "test_account",
+                "password": "123456"
+            })
+        self.assertEqual(response.status_code, 200)
