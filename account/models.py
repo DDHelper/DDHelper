c4b04a607f02eb0cea47879ac075db28f6a21f40
EdrowsLuo <793971297@qq.com>
2021-11-30 17:53:58 +0800

    将email设置为唯一且必须的

--- account/models.py
+++ account/models.py
@@ -5,6 +5,7 @@ from django.db import models
 
 class Userinfo(AbstractUser):
     uid = models.BigAutoField(primary_key=True)
+    email = models.EmailField(unique=True)
 
     def __str__(self):
         return str(self.uid)
