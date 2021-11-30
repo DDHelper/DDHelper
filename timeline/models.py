e5fa767b4633feaf78e55733b025b4d3cd848b57
nagi2330 <humingzhong00@126.com>
2021-11-30 22:36:22 +0800

    建立timeline的app并添加对应model

---
+++ timeline/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+from dynamic.models import Dynamic
+# Create your models here.
+
+
+class TimelineEntry(models.Model):
+    # 动态摘要信息
+    text = models.JSONField()
+    # 对应的原动态
+    dynamic = models.OneToOneField(
+        Dynamic,
+        on_delete=models.CASCADE,
+        primary_key=True)
+    # 提取的动态含有的时间信息
+    event_time = models.DateTimeField()
+    # 动态类型类别
+    EVENT_TYPE = [
+        ('NT', 'Non-time'),
+        ('ST', 'Stream'),
+        ('LO', 'Lottery'),
+        ('RE', 'Release')
+    ]
+    type = models.CharField(
+        max_length=2,
+        choices=EVENT_TYPE,
+        default='NT'
+    )
---
+++ timeline/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+import re
+
+# Create your views here.
