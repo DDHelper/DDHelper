ebef90415714449dcf75820876b436f18511a9b9
EdrowsLuo <793971297@qq.com>
2021-12-03 12:32:49 +0800

    修改dynamic.models结构，以及相关代码

--- dynamic/models.py
+++ dynamic/models.py
@@ -1,29 +1,29 @@
 from django.db import models
 
+from subscribe.models import SubscribeMember
 
-class Member(models.Model):
+
+class DynamicMember(models.Model):
     # b站mid
-    mid = models.BigIntegerField(primary_key=True)
-    # b站用户名
-    name = models.CharField(max_length=50)
-    # 头像url
-    face = models.URLField(max_length=200)
-    # 最近用户数据更新时间（头像、昵称)
-    last_profile_update = models.DateTimeField()
+    mid = models.OneToOneField(SubscribeMember, on_delete=models.CASCADE, primary_key=True)
     # 最近更新动态页面的时间
-    last_dynamic_update = models.DateTimeField(null=True)
+    last_dynamic_update = models.DateTimeField(auto_now=True)
 
 
 class Dynamic(models.Model):
     # 动态id
     dynamic_id = models.BigIntegerField(primary_key=True)
     # 发动态的b站用户
-    member = models.ForeignKey(Member, on_delete=models.CASCADE)
+    member = models.ForeignKey(SubscribeMember, on_delete=models.CASCADE)
     # 动态的种类，参考：https://github.com/SocialSisterYi/bilibili-API-collect/blob/master/dynamic/get_dynamic_detail.md
     dynamic_type = models.IntegerField()
     # 动态的时间戳
     timestamp = models.DateTimeField()
     # 原始动态数据
     raw = models.JSONField()
+
+    @classmethod
+    def select_dynamics_in_group(cls, group):
+        return Dynamic.objects.filter(member__in=group.members.all())
 
 
--- dynamic/tasks.py
+++ dynamic/tasks.py
@@ -19,7 +19,7 @@ def call_full_sync(chunk_size=5):
     :return:
     """
     # TODO: 加锁，防止重复发送
-    all_mid = list(models.Member.objects.values_list("mid", flat=True))
+    all_mid = list(models.DynamicMember.objects.values_list("mid_id", flat=True))
     chunked = sync_member.chunks(zip(all_mid), chunk_size).group()
     chunked.apply_async()
     logger.info(f"执行全动态同步，发出{len(chunked)}个任务")
@@ -33,37 +33,36 @@ def add_member(mid: int, initial_sync=True):
     :param initial_sync: 是否自动创建初始化同步任务
     :return:
     """
-    member = dsync.get_member(mid)
+    member = dsync.get_subscribe_member(mid)
     if member is None:
-        member = models.Member(mid=mid)
-        msg = dsync.update_member_profile(member)
-        if msg:
-            # 更新信息失败
-            logger.warning(f"更新信息失败: mid={mid} msg={msg}")
-            raise Exception(f"更新信息失败: mid={mid} msg={msg}")
-        member.save()
+        logger.warning(f"尝试添加关注系统中不存在的成员: mid={mid}")
+        raise Exception(f"尝试添加关注系统中不存在的成员: mid={mid}")
+    dynamic_member = dsync.get_dynamic_member(mid)
+    if dynamic_member is None:
+        dynamic_member = models.DynamicMember(mid=member)
+        dynamic_member.save()
         logger.info(f"添加新成员: mid={mid}")
 
         if initial_sync:
-            sync_member.delay(mid)
+            sync_member.delay(mid, force_update=True)
 
 
 @shared_task
-def sync_member(mid: int, min_interval=600):
+def sync_member(mid: int, min_interval=600, force_update=False):
     """
     同步某个成员最近的动态
     :param mid: 成员
     :param min_interval: 最小更新间隔，如果离上次更新小于这个间隔会直接跳过这次更新
+    :param force_update: 是否强制更新
     :return:
     """
     logger.info(f"开始执行成员{mid}的动态同步")
-    member = dsync.get_member(mid)
+    member = dsync.get_subscribe_member(mid)
     if member is None:
         logger.warning(f"成员{mid}不存在")
         raise Exception()
 
-    if member.last_dynamic_update is not None and \
-            (timezone.now() - member.last_profile_update).seconds <= min_interval:
+    if not force_update and (timezone.now() - member.dynamicmember.last_dynamic_update).seconds <= min_interval:
         # 如果离上次更新小于这个间隔会直接跳过这次更新
         logger.info(f"成员{mid}在{min_interval}s内进行过同步，跳过此任务")
         return
@@ -75,7 +74,6 @@ def sync_member(mid: int, min_interval=600):
         logger.warning(f"拉取动态失败: mid={mid} msg={msg}")
         raise Exception(f"拉取动态失败: mid={mid} msg={msg}")  # TODO: 添加retry机制
 
-    member.last_dynamic_update = timezone.now()
     if len(dynamics) != 0:
         dynamics.sort(key=lambda d: d.dynamic_id)  # 强制从小到大的顺序进行提交
 
@@ -85,6 +83,8 @@ def sync_member(mid: int, min_interval=600):
             for dy in dynamics:
                 dy.save()
             member.save()
+            member.dynamicmember.last_dynamic_update = timezone.now()
+            member.dynamicmember.save()
             logger.info(f"成员{mid}动态同步完成")
     except DatabaseError as e:
         # 更新失败
