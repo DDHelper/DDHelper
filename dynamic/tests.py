68953f871f405f4579ed7ce6ab2c8670e5bd930f
EdrowsLuo <793971297@qq.com>
2021-12-03 12:33:19 +0800

    修改dynamic.models相关的test

--- dynamic/tests.py
+++ dynamic/tests.py
@@ -5,22 +5,28 @@ from . import models
 from . import dsync
 from . import tasks
 
+from subscribe.models import SubscribeMember, MemberGroup
+
 CST_TIME_ZONE = pytz.timezone("Asia/Shanghai")
 
 
 class ModelTest(TestCase):
     def test_model_and_time(self):
-        m = models.Member()
-        m.mid = 1
-        m.name = "t"
-        m.face = "http://aa.www/a.png"
-        m.last_profile_update = timezone.now()
+        sm = SubscribeMember()
+        sm.mid = 1
+        sm.name = "t"
+        sm.face = "http://aa.www/a.png"
+        sm.last_profile_update = timezone.now()
+        sm.save()
+
+        m = models.DynamicMember()
+        m.mid = sm
         m.last_dynamic_update = timezone.now()
         m.save()
 
         d = models.Dynamic()
         d.dynamic_id = 1
-        d.member = m
+        d.member = sm
         d.dynamic_type = 233
         d.timestamp = timezone.datetime.fromtimestamp(1636009208, tz=CST_TIME_ZONE)
         d.raw = {"a": 1}
@@ -38,25 +44,24 @@ class ModelTest(TestCase):
 
 class DsyncTest(TestCase):
     def test_dsync(self):
-        member = models.Member(mid=8401607)
+        member = SubscribeMember(mid=8401607)
         dsync.update_member_profile(member)
-        member.save()
 
-        member = dsync.get_member(8401607)
+        member = dsync.get_subscribe_member(8401607)
         self.assertEqual(member.name, "无米酱Official")
-        self.assertEqual(dsync.get_member(1), None)
+        self.assertEqual(dsync.get_subscribe_member(1), None)
 
         self.assertEqual(dsync.get_saved_latest_dynamic(1), None)
 
     def test_task(self):
         mid = 8401607
-        tasks.add_member(mid, initial_sync=False)  # 直接调用celery task时与一般的函数调用行为相同
+        member = SubscribeMember(mid=mid)
+        dsync.update_member_profile(member)
 
-        member = dsync.get_member(mid)
+        tasks.add_member.delay(mid)
+
+        member = dsync.get_subscribe_member(mid)
         self.assertEqual(member.name, "无米酱Official")
-
-        self.assertEqual(dsync.get_saved_latest_dynamic(mid), None)
-        tasks.sync_member(member.mid)
         self.assertNotEqual(dsync.get_saved_latest_dynamic(mid), None)
         self.assertGreater(len(models.Dynamic.objects.all()), 0)
 
