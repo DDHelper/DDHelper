f0e4524189f76d47d4229945394abd905bf1854a
nagi2330 <humingzhong00@126.com>
2021-12-23 19:40:15 +0800

    timeline模块views的注释

--- timeline/views.py
+++ timeline/views.py
@@ -13,9 +13,22 @@ from django.http.response import JsonResponse
 @login_required
 def show_timeline(request):
     """
-    类似dynamic中的实现
-    :param request:
-    :return:
+    根据筛选条件展示符合要求的timeline
+    :param request: GET型request
+    请求参数
+        gid	num 非必要 需要展示的timeline所在组id,不传入时显示当前用户默认分组
+        offset num 非必要 timeline时间筛选的最新时间戳
+        size num 非必要 请求的timeline数量，默认20条
+    :return: Response
+    返回数据
+        data object
+        - has_more bool 是否有符合条件但因为超出请求数量而为加载的timeline
+        - gid num 所选取分组id
+        - group_name string 所选取分组的名称
+        - offset num 返回的timeline对象中最旧的时间戳
+        - data object_array 各timeline的信息
+        code integer 状态码
+        msg string 出错信息
     """
     with load_params():
         gid = int(request.GET.get('gid', 0))
