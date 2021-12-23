016e7e6a3b6db536d349bfe6aafa665278c38c91
EdrowsLuo <793971297@qq.com>
2021-12-23 13:14:13 +0800

    添加mock功能

--- DDHelper/util.py
+++ DDHelper/util.py
@@ -9,3 +9,16 @@ def load_params():
         yield
     except (KeyError, ValueError):
         raise BadRequest()
+
+
+@contextmanager
+def mock(target, **kwargs):
+    old_values = {}
+    for n in kwargs:
+        old_values[n] = getattr(target, n)
+        setattr(target, n, kwargs[n])
+    try:
+        yield
+    finally:
+        for n in old_values:
+            setattr(target, n, old_values[n])
