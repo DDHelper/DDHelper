d925292f86ccd23027639158acc6e1cb94fae681
nagi2330 <humingzhong00@126.com>
2021-12-23 19:21:20 +0800

    timeline模块models的注释

--- timeline/models.py
+++ timeline/models.py
@@ -17,22 +17,23 @@ class TimelineDynamicProcessInfo(models.Model):
     @classmethod
     def get(cls, dynamic_id):
         """
-
-        :rtype: TimelineDynamicProcessInfo
+        按id获取某个timeline处理过程
+        :param dynamic_id: 需要获取的处理过程id
+        :return: TimelineDynamicProcessInfo 获取到的分组
         """
         return TimelineDynamicProcessInfo.objects.get_or_create(dynamic_id=dynamic_id)[0]
 
     def should_update(self):
         """
-        如果数据库中记录的version小于当前的version，表示动态处理系统更新，需要重新处理
-        :return:
+        返回当前动态是否需要更新
+        :return: Bool 需要更新返回True，否则返回False
         """
         return self.process_version < TimelineDynamicProcessInfo.VERSION
 
     def apply_update(self):
         """
-        当动态处理完后更新版本
-        :return:
+        动态更新完后更新版本号
+        :return: None
         """
         self.process_version = TimelineDynamicProcessInfo.VERSION
         self.save()
@@ -48,7 +49,7 @@ class TimelineEntry(models.Model):
         primary_key=True)
     # 提取的动态含有的时间信息
     event_time = models.DateTimeField()
-    # 动态类型类别
+    # 动态类型类别，依次是位置类别、直播类别、抽奖类别、视频投稿类别
     EVENT_TYPE = [
         ('UN', 'Unknown'),
         ('ST', 'Stream'),
@@ -61,6 +62,7 @@ class TimelineEntry(models.Model):
         default='NT'
     )
 
+    # 声明类在数据库中的排序方式和索引便于加速查找
     class Meta:
         ordering = ['-event_time']
         indexes = [models.Index(fields=['event_time'])]
@@ -69,9 +71,9 @@ class TimelineEntry(models.Model):
     def select_entry_in_group(cls, group, offset):
         """
         选择一个分组中的timeline对象，从时间小于time_end算起
-        :param group:
-        :param offset:
-        :return:
+        :param group: 要选择的timeline对象所在的分组
+        :param offset: 时间上的最大值
+        :return: QuerySet 包含所有满足条件的timeline
         """
         query = TimelineEntry.objects.filter(dynamic__member__in=list(group.members.all().values_list('mid', flat=True)))
         if offset is not None:
@@ -79,6 +81,10 @@ class TimelineEntry(models.Model):
         return query.order_by('-event_time')
 
     def as_dict(self):
+        """
+        返回timeline对象作为文本输出的信息
+        :return: dict timeline的基本信息
+        """
         return {
             'dynamic_id': self.dynamic_id,
             'text': self.text,
@@ -88,4 +94,8 @@ class TimelineEntry(models.Model):
         }
 
     def __str__(self):
+        """
+        返回timeline对象作为文本输出的信息
+        :return: string timeline的基本信息以字典文本串形式输出
+        """
         return str(self.as_dict())
